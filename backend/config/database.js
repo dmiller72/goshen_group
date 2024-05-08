314fbf8e11aa38651f4e444140f35da201d7298e
David Miller <mtech.websites@gmail.com>
2024-05-08 14:05:48 -0500

    modified dabase.js to pull db name and password from env file instead of being hard coded. (David's is in the frontend folder)

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,9 +16,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "goshen_group"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "lifetech"),
+      database: env("DATABASE_NAME", DATABASE_NAME),
+      user: env("DATABASE_USERNAME", DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD"),
     },
     useNullAsDefault: true,
   },
