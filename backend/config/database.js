6e040a8a009ccb9928bde71f5fa22d4dcd600029
David Miller <mtech.websites@gmail.com>
2024-05-08 14:32:19 -0500

    changed database.js again to make all key-value pairs dynamic. env variables now in backend folder

--- backend/config/database.js
+++ backend/config/database.js
@@ -14,11 +14,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", DATABASE_NAME),
-      user: env("DATABASE_USERNAME", DATABASE_USERNAME),
-      password: env("DATABASE_PASSWORD", DATABASE_PASSWORD),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
     },
     useNullAsDefault: true,
   },
